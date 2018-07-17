4bc05a68e1b2622cd7731061fc62ae02d2e15bfa
Eskil Opdahl Nordland <eskil.nordland@gmail.com>
2018-07-17 02:29:42 +0200

    Registry docs.

--- src/js/domreplay/registry.js
+++ src/js/domreplay/registry.js
@@ -12,6 +12,11 @@ class RegistrySingleton {
 		}
 	}
 
+	/**
+	 * Register an event.
+	 * Object passed should be an instance of EventBaseClass.
+	 * @param {EventBaseClass} event
+	 */
 	registerEvent(event) {
 		if (!(event instanceof EventBaseClass)) {
 			throw new ProgrammingError('RegistrySingleton should only contain classes with EventBaseClass as superclass');
@@ -19,10 +24,20 @@ class RegistrySingleton {
 		this._events.set(event.eventType, event);
 	}
 
+	/**
+	 * Gets an event from registry by event type.
+	 * @param {String} eventType
+	 * @returns {String | undefined}
+	 */
 	getEvent(eventType) {
 		return this._events.get(eventType);
 	}
 
+	/**
+	 * Gets a list of events by tagname
+	 * @param {String} tagname
+	 * @returns {Array[]}
+	 */
 	getEventsByTagname(tagname) {
 		let events = []
 		for (let event of this._events.values()) {
@@ -33,18 +48,33 @@ class RegistrySingleton {
 		return events;
 	}
 
+	/**
+	 * Delete an event from registry by event type.
+	 * @param {String} eventType
+	 */
 	deleteEvent(eventType) {
 		this._events.delete(eventType);
 	}
 
+	/**
+	 * Clears all events from registry.
+	 */
 	clearAllEvents() {
 		this._events.clear();
 	}
 
+	/**
+	 * Gets event types in registry.
+	 * @returns {IterableIterator<String>}
+	 */
 	getEventTypes() {
 		return this._events.keys();
 	}
 
+	/**
+	 * Gets tag names in registry.
+	 * @returns {Array}
+	 */
 	getTagnames() {
 		let tagnames = []
 		for (let event of this._events.values()) {
@@ -53,16 +83,28 @@ class RegistrySingleton {
 		return tagnames;
 	}
 
+	/**
+	 * Gets registry size
+	 * @returns {number}
+	 */
 	get size() {
 		return this._events.size;
 	}
 
+	/**
+	 * Sets trail function for all events in registry.
+	 * @param {function} trailFunc
+	 */
 	setTrailFuncForAllEventObjectsInRegistry(trailFunc) {
 		for (let event of this._events.values()) {
 			event.trailFunc = trailFunc;
 		}
 	}
 
+	/**
+	 * Sets tracker function for all events in registry.
+ 	 * @param {function} trackerFunc
+	 */
 	setTrackerFuncForAllEventObjectsInRegistry(trackerFunc) {
 		for (let event of this._events.values()) {
 			event.trackerFunc = trackerFunc
